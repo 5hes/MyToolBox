d8f25fedc275685f4408fc24b903e8caaa8c1a8e
5hes <553919500@qq.com>
2024-11-26 22:16:49 +0800

    Update merge.py

--- FontTools/MergeFont/merge.py
+++ FontTools/MergeFont/merge.py
@@ -5,21 +5,21 @@ def merge_fonts(source_font_path, eng_font_path, merged_font_path):
     font1 = TTFont(source_font_path)
     font2 = TTFont(eng_font_path)
 
-    # 合并字体
-    for table in font2.keys():
-        if table not in font1.keys():
-            font1[table] = font2[table]
-        else:
-            # 处理 glyf 表
-            if table == "glyf":
-                for glyph in font2["glyf"].keys():
-                    # 如果 glyph 不在 font1 中，添加它
-                    if glyph not in font1["glyf"].keys():
-                        font1["glyf"][glyph] = font2["glyf"][glyph]
-                    else:
-                        # 如果 glyph 已存在，您可以选择覆盖或跳过
-                        # 这里选择覆盖
-                        font1["glyf"][glyph] = font2["glyf"][glyph]
+    # 合并 glyf 表
+    if "glyf" in font2.keys():
+        for glyph in font2["glyf"].keys():
+            if glyph not in font1["glyf"].keys():
+                font1["glyf"][glyph] = font2["glyf"][glyph]
+            else:
+                font1["glyf"][glyph] = font2["glyf"][glyph]
+
+    # 合并 hmtx 表
+    if "hmtx" in font2.keys():
+        for glyph in font2["hmtx"].metrics.keys():
+            if glyph not in font1["hmtx"].metrics.keys():
+                font1["hmtx"].metrics[glyph] = font2["hmtx"].metrics[glyph]
+            else:
+                font1["hmtx"].metrics[glyph] = font2["hmtx"].metrics[glyph]
 
     # 保存合并后的字体
     font1.save(merged_font_path)
