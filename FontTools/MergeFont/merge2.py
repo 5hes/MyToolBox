641ce096f30d9b0b9156ea650d4dd2ddd327a094
5hes <553919500@qq.com>
2024-11-26 22:33:12 +0800

    Create merge2.py

---
+++ FontTools/MergeFont/merge2.py
@@ -0,0 +1,16 @@
+from fontTools.ttLib import TTFont
+from fontTools.merge import merge
+
+def merge_fonts(source_font_path, eng_font_path, merged_font_path):
+    font1 = TTFont(source_font_path)
+    font2 = TTFont(eng_font_path)
+
+    # 使用 merge 函数合并字体
+    merged_font = merge([font1, font2])
+
+    # 保存合并后的字体
+    merged_font.save(merged_font_path)
+    print(f"Fonts merged and saved to {merged_font_path}")
+
+# 使用示例
+merge_fonts("SourceHanSansCN-Regular-nohint.ttf", "eng.ttf", "merged_font2.ttf")
