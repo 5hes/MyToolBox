b4c7b0f5d50e6ff66328106a80f5818293c8f971
5hes <553919500@qq.com>
2024-11-28 17:48:42 +0800

    Update env.sh

--- Download/env.sh
+++ Download/env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export LINK=$(cat link) && rm link
 export TARGET=$(cat target) && rm target
+export EXCLUDE=$(cat exclude) && rm exclude
 export EXTRA=true
 echo "TARGET=$TARGET" >> $GITHUB_ENV
