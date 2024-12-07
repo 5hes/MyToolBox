e0e9854d21821d1b758db56328d970af3b4aea50
5hes <553919500@qq.com>
2024-12-07 10:28:53 +0800

    Update merge2.py

--- FontTools/MergeFont/merge2.py
+++ FontTools/MergeFont/merge2.py
@@ -13,4 +13,4 @@ def merge_fonts(source_font_path, eng_font_path, merged_font_path):
     print(f"Fonts merged and saved to {merged_font_path}")
 
 # 使用示例
-merge_fonts("SourceHanSansCN-Regular-nohint.ttf", "eng.ttf", "merged_font2.ttf")
+merge_fonts("cjk.ttf", "eng.ttf", "merged_font2.ttf")
